f6f645278fec6d78b666dba8e5f986ddf74df473
dresa91 <andreas.krey@nordakademie.de>
2014-10-28 15:45:58 +0100

    Erweiterung der LogInAction für Abfrage mit DB.

--- Cars/src/de/nak/studentsdatabase/action/LogInAction.java
+++ Cars/src/de/nak/studentsdatabase/action/LogInAction.java
@@ -1,11 +1,15 @@
 package de.nak.studentsdatabase.action;
 
+import java.util.List;
 import java.util.Map;
 
 import org.apache.struts2.interceptor.SessionAware;
 
+import com.opensymphony.xwork2.Action;
 import com.opensymphony.xwork2.ActionSupport;
 
+import de.nak.studentsdatabase.model.User;
+import de.nak.studentsdatabase.service.UserService;
 
 /**
  * Action for login.
@@ -15,47 +19,78 @@ import com.opensymphony.xwork2.ActionSupport;
  */
 
 @SuppressWarnings("serial")
-public class LogInAction extends ActionSupport implements SessionAware {
-    private String userName;
-    private Map<String, Object> session;
-    
-    /**
-     * Method for log out.
-     * @return
-     */
-    public String logOut() {
-            session.remove("loginId");
-            addActionMessage("You have been Successfully Logged Out");
-            return SUCCESS;
-    }
+public class LogInAction extends ActionSupport implements SessionAware, Action {
+	/** User name value of input. */
+	private String userName;
+	/** Password value of input. */
+	private String Password;
+	/** List of users */
+	private List<User> userList;
+	/** The user service */
+	private UserService userService;
+	/** Map for session */
+	private Map<String, Object> session;
 
-    /**
-     * Method for log in.
-     * @return
-     */
-    public String login() {
-            if (userName.isEmpty()) {
-                    addActionError("Please insert a user name!");
-                    return LOGIN;
-            } else {
-                    session.put("loginId", userName);
-                    return SUCCESS;
-            }
-    }
+	/**
+	 * Method for log in.
+	 * 
+	 * @return
+	 */
+	public String login() throws Exception {
+		userList = userService.loadAll();
+		
+		
+		if (userName.isEmpty()) {
+			addActionError("Please insert a user name!");
+			return LOGIN;
+		} else {
+			session.put("loginId", userName);
+			return SUCCESS;
 
-    public String getUserName() {
-            return userName;
-    }
+		}
+	}
 
-    public Map<String, Object> getSession() {
-            return session;
-    }
+	/**
+	 * Method for log out.
+	 * 
+	 * @return
+	 */
+	public String logOut() {
+		session.remove("loginId");
+		addActionMessage("You have been Successfully Logged Out");
+		return SUCCESS;
+	}
 
-    public void setUserName(String userName) {
-            this.userName = userName;
-    }
+	public String getUserName() {
+		return userName;
+	}
 
-    public void setSession(Map<String, Object> map) {
-            this.session = map;
-    }
+	public Map<String, Object> getSession() {
+		return session;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public void setSession(Map<String, Object> map) {
+		this.session = map;
+	}
+
+	public String getPassword() {
+		return Password;
+	}
+
+	public void setPassword(String password) {
+		Password = password;
+	}
+
+	public List<User> getUserList() {
+		return userList;
+	}
+
+	public void setUserService(UserService userService) {
+		this.userService = userService;
+	}
+
 }
